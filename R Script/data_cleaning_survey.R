ddcdaff6037570c2b2f0c440f05afa4a1483e58a
Qihui Huang <haileyhuang04@outlook.com>
2020-10-28 15:21:46 -0400

    qh

--- R Script/data_cleaning_survey.R
+++ R Script/data_cleaning_survey.R
@@ -105,9 +105,6 @@ reduced_data_survey <- reduced_data_survey %>%
     education == "Middle School - Grades 4 - 8" ~ "Grade 5, 6, 7, or 8",
     education == "3rd Grade or less" ~ "N/A or no schooling"))
 
-
-
-
 # rename
 reduced_data_survey <- 
   reduced_data_survey %>% 
